29391fe12da14520787d6c11b469302e7cfe0699
Venkat R <95846720+venkat0086@users.noreply.github.com>
2022-04-28 23:22:30 +0530

    Updated

---
+++ JS-revision2/toggle/toggle.js
@@ -0,0 +1,19 @@
+function toggler() {
+  var arr = arguments;
+  var index = -1;
+
+  return function () {
+    index = index + 1;
+    if (index >= arr.length) {
+      index = 0;
+    }
+    return arr[index];
+  };
+}
+const toggle = toggler(1, 2, 3, 4);
+
+console.log(toggle());
+console.log(toggle());
+console.log(toggle());
+console.log(toggle());
+console.log(toggle());
